1a1c529f5b775074ff15f49759b9c3dd78a626e5
Linus Walker <rissk_it@proton.me>
2025-04-04 10:24:01 +0630

    chore: neovim shits

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -21,20 +21,29 @@ require("lazy").setup({
     branch = "v2.5",
     import = "nvchad.plugins",
   },
-
+  { "cordx56/rustowl", dependencies = { "neovim/nvim-lspconfig" } },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    "dariuscorvus/tree-sitter-surrealdb.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false, -- This makes sure it loads immediately
+    ft = { "surql", "surrealql" },
+    config = function()
+      require("tree-sitter-surrealdb").setup()
+    end,
+  },
   {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
-  },
-  -- add this to the file where you setup your other plugins:
-  {
-    "monkoose/neocodeium",
-    config = function()
-      local neocodeium = require "neocodeium"
-      neocodeium.setup()
-      vim.keymap.set("i", "<A-f>", neocodeium.accept)
-    end,
   },
   -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
   {
@@ -53,6 +62,12 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+
   { import = "plugins" },
 }, lazy_config)
 
@@ -63,7 +78,18 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "nvchad.autocmds"
 
+require("nvim-treesitter.parsers").get_parser_configs().just = {
+  install_info = {
+    url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+    files = { "src/parser.c", "src/scanner.c" },
+    branch = "main",
+    -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
+  },
+  maintainers = { "@IndianBoy42" },
+}
+
 require("Comment").setup()
+require("typescript-tools").setup {}
 
 -- Bind <leader>cc to comment the selected lines or current line
 vim.keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle comment (current line)" })
@@ -74,46 +100,9 @@ vim.keymap.set(
   { desc = "Toggle comment (visual selection)" }
 )
 
--- NeoCodeium Configuration
-require("neocodeium").setup {
-  -- If `false`, then would not start codeium server (disabled state)
-  -- You can manually enable it at runtime with `:NeoCodeium enable`
-  enabled = true,
-  -- Path to a custom Codeium server binary (you can download one from:
-  -- https://github.com/Exafunction/codeium/releases)
-  bin = nil,
-  -- When set to `true`, autosuggestions are disabled.
-  -- Use `require'neodecodeium'.cycle_or_complete()` to show suggestions manually
-  manual = false,
-  -- Information about the API server to use
-  server = {
-    -- API URL to use (for Enterprise mode)
-    api_url = nil,
-    -- Portal URL to use (for registering a user and downloading the binary)
-    portal_url = nil,
-  },
-  -- Set to `false` to disable showing the number of suggestions label in the line number column
-  show_label = true,
-  -- Set to `true` to enable suggestions debounce
-  debounce = false,
-  -- Maximum number of lines parsed from loaded buffers (current buffer always fully parsed)
-  -- Set to `0` to disable parsing non-current buffers (may lower suggestion quality)
-  -- Set it to `-1` to parse all lines
-  max_lines = 10000,
-  -- Set to `true` to disable some non-important messages, like "NeoCodeium: server started..."
-  silent = false,
-  -- Set to `false` to disable suggestions in buffers with specific filetypes
-  -- You can still enable disabled by this option buffer with `:NeoCodeium enable_buffer`
-  filetypes = {
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    ["."] = false,
-  },
-  -- List of directories and files to detect workspace root directory for Codeium chat
-  root_dir = { ".bzr", ".git", ".hg", ".svn", "_FOSSIL_", "package.json", "Cargo.toml" },
-}
+local ft = require "Comment.ft"
 
+ft({ "surrealql", "surql" }, "#%s")
 -- Go to next diagnostic with <leader>j
 vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_next)
 
@@ -126,3 +115,9 @@ vim.keymap.set("n", "<leader>dq", ":bd<CR>", { noremap = true, silent = true })
 vim.schedule(function()
   require "mappings"
 end)
+
+vim.filetype.add {
+  extension = {
+    surql = "surql",
+  },
+}
